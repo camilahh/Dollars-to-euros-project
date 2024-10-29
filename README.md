14d2274f86339dd74ee637eb5a4149dd4238148a
camilahh <camilann34@gmail.com>
2024-10-29 11:18:15 -0500

    added first part which makes dollars to euros

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Dollars-to-euros-project
+
+"""
+This program converts dollars to euros
+Author: Camila Coleman
+Version 1.0
+"""
+
+
---
+++ code.py
@@ -0,0 +1,7 @@
+
+dollar = float(input("enter dollar amount to be convert: "))
+euro = dollar * .94540
+
+convert = input("would you like to convert your dollars to euros? ")
+if input == "yes":
+    print("you will have " + str(euro) + " euros" )
