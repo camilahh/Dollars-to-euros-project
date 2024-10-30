ddf733d34f864ceb52848a961d5b301cfa5f8871
camilahh <camilann34@gmail.com>
2024-10-30 10:44:41 -0500

    added if else statement

--- code.py
+++ code.py
@@ -3,5 +3,8 @@ dollar = float(input("enter dollar amount to be convert: "))
 euro = dollar * .94540
 
 convert = input("would you like to convert your dollars to euros? ")
-if input == "yes":
+if convert == "yes":
     print("you will have " + str(euro) + " euros" )
+else:
+    print("ok then...") 
+    exit()
