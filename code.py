37d1130d405bb88ebb0a1ecfd0f45c360eba5ff7
camilahh <camilann34@gmail.com>
2024-10-30 10:55:40 -0500

    improved code

--- code.py
+++ code.py
@@ -1,10 +1,14 @@
 
-dollar = float(input("enter dollar amount to be convert: "))
-euro = dollar * .94540
 
-convert = input("would you like to convert your dollars to euros? ")
-if convert == "yes":
+
+convert = input("would you like to convert your dollars to euros? (y/n) ")
+
+while convert == "y":
+    
+    dollar = float(input("enter dollar amount to be convert: "))
+    euro = dollar * .94540
     print("you will have " + str(euro) + " euros" )
-else:
-    print("ok then...") 
-    exit()
+    convert = input("would you like to convert your dollars to euros? (y/n) ")
+
+print("ok then...") 
+
